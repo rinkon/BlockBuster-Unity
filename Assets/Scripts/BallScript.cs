128d50c841a24b441d445d5ac7d8f4120b4cfed4
ashik aowal <ashik.aowal@goava.com>
2020-12-01 22:42:00 +0600

    same height for long issue fixed with down force 6

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -7,6 +7,9 @@ public class BallScript : MonoBehaviour
     private Rigidbody2D _rb;
     [SerializeField]
     private float _ballSpeed = 10.0f;
+    private float prevY = 0.0f;
+    private float curY = 0.0f;
+    private float sameYDurration = 0.0f;
 
     private void Awake() {
         _rb = GetComponent<Rigidbody2D>();    
@@ -19,5 +22,20 @@ public class BallScript : MonoBehaviour
     private void Update()
     {
         _rb.velocity = _rb.velocity.normalized * _ballSpeed;
+        
+        curY = transform.position.y;
+        if(prevY == curY){
+            sameYDurration += Time.deltaTime;
+        }
+        else{
+            sameYDurration = 0.0f;
+            prevY = curY;
+        }
+        if(sameYDurration > 5.0f){
+            print("same height for long");
+            _rb.AddForce(new Vector3(0f, -6.0f, 0.0f));
+            sameYDurration = 0;
+        }
+        
     }
 }
